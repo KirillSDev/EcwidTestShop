e8fd13c1b918c95c0d31a721e424ed25ddb9dc27
Kirill Starodubsev <colereteps@gmail.com>
2023-03-21 18:29:38 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import styles from './styles.module.scss'
+export default {}
+</script>
+
+<template>
+	<div>Hello</div>
+</template>
+
+<style module></style>
