f870060c311f6e1e4150f23efd2ec5c14f8cdb29
Kirill Starodubsev <colereteps@gmail.com>
2023-03-22 18:40:07 +0400

    added alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@/': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@scss': path.resolve(__dirname, 'src/assets/scss'),
       '@store': path.resolve(__dirname, 'src/store'),
