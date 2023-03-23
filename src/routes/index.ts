9e43be969c3ab27ae39d0b6872cbb99c861a1e65
Kirill Starodubsev <colereteps@gmail.com>
2023-03-23 23:12:04 +0400

    added some routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,28 @@
 import ButtonVue from '@components/Button.vue'
 import HomePage from '@pages/HomePage.vue'
 import CategoryPage from '@pages/CategoryPage.vue'
-import { createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import DetailPage from '@pages/DetailPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/category/:categoryId',
-        name: 'category',
-        component: CategoryPage
-    },
-    {
-        path: '/',
-        name: 'home',
-        component: HomePage
-    }
+	{
+		path: '/category/:categoryId',
+		name: 'category',
+		component: CategoryPage
+	},
+	{
+		path: '/',
+		name: 'home',
+		component: HomePage
+	},
+	{
+		path: '/product/:id',
+		name: 'product',
+		component: DetailPage
+	}
 ]
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes
+	history: createWebHistory(),
+	routes
 })
