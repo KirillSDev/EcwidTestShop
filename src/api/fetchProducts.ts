48b63f1b2bb7d54fc260259042a89ba584ff1d86
Kirill Starodubsev <colereteps@gmail.com>
2023-03-23 23:25:24 +0400

    added fetchProduct

--- src/api/fetchProducts.ts
+++ src/api/fetchProducts.ts
@@ -4,18 +4,31 @@ const STORE_ID = import.meta.env.VITE_STORE_ID
 const TOKEN = import.meta.env.VITE_TOKEN
 
 export const fetchProducts = async () => {
-    try {
-        const response = await axios({
-            method: 'get',
-            url: `${STORE_ID}/products`,
-            headers: {
-                Authorization: 'Bearer ' +  TOKEN
-            }
-        })
-        return response
-    } catch (error) {
-        console.error(error)
-    }
+	try {
+		const response = await axios({
+			method: 'get',
+			url: `${STORE_ID}/products`,
+			headers: {
+				Authorization: 'Bearer ' + TOKEN
+			}
+		})
+		return response
+	} catch (error) {
+		console.error(error)
+	}
 }
 
-
+export const fetchProduct = async (productId: number) => {
+	try {
+		const response = axios({
+			method: 'get',
+			url: `${STORE_ID}/products/${productId}`,
+			headers: {
+				Authorization: 'Bearer ' + TOKEN
+			}
+		})
+		return response
+	} catch (error) {
+		console.error(error)
+	}
+}
