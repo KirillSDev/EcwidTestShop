717f9d1b15e21c649512044a7a585b168141f1f4
Kirill Starodubsev <colereteps@gmail.com>
2023-03-23 15:16:29 +0400

    added HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+	<div :class="$style.main">
+		<CategoriesContainer />
+		<ProductsContainer />
+		<div
+			:class="$style.loading"
+			v-if="!(categoriesStore.status && productsStore.status)"
+		>
+			<Loading />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import Loading from '@components/Loading.vue'
+import CategoriesContainer from '@containers/CategoriesContainer.vue'
+import ProductsContainer from '@containers/ProductsContainer.vue'
+import { useProductsStore } from '@store/ProductsStore'
+import { useCategoriesStore } from '@store/CategoriesStore'
+import { ref } from 'vue'
+const categoriesStore = useCategoriesStore()
+const productsStore = useProductsStore()
+</script>
+
+<style lang="scss" module>
+@import '@scss/colors.scss';
+.main {
+	background-color: $light;
+	overflow: scroll;
+	overflow-x: hidden;
+	padding: 20px;
+	height: calc(100vh - 70px);
+}
+.loading {
+	display: flex;
+	justify-content: center;
+}
+</style>
