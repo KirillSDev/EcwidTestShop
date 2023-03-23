c9113f86895c4ecefafc4a82b5220e07ac486a29
Kirill Starodubsev <colereteps@gmail.com>
2023-03-23 23:11:13 +0400

    added some actions
    added: setCurrentProduct in ProductsStore
    added: setCurrentCategory in CategoriesStore

--- src/store/CategoriesStore.ts
+++ src/store/CategoriesStore.ts
@@ -1,21 +1,25 @@
-import { defineStore } from "pinia";
-import { fetchCategories } from "@api/fetchCategories";
-import { ref } from "vue";
-import { ICategory } from "@interfaces/Category.interface";
-
+import { defineStore } from 'pinia'
+import { fetchCategories } from '@api/fetchCategories'
+import { ref } from 'vue'
+import { ICategory } from '@interfaces/Category.interface'
 
 export const useCategoriesStore = defineStore('categoriesStore', () => {
-    const categories = ref([] as ICategory[]) 
-    const status = ref(false)
-    const getCategories = async () => {
-        const response = await fetchCategories()
-        const data = await response?.data?.items
-        categories.value = data
-        status.value = true
-    }
-    return {
-        categories,
-        status,
-        getCategories
-    }
+	const categories = ref([] as ICategory[])
+	const currentCategory = ref({} as ICategory)
+	const status = ref(false)
+	const setCurrentCategory = (category: ICategory) => {
+		currentCategory.value = category
+	}
+	const getCategories = async () => {
+		const response = await fetchCategories()
+		const data = await response?.data?.items
+		categories.value = data
+		status.value = true
+	}
+	return {
+		categories,
+		status,
+		getCategories,
+		setCurrentCategory
+	}
 })
--- src/store/ProductsStore.ts
+++ src/store/ProductsStore.ts
@@ -1,21 +1,25 @@
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { fetchProducts } from '@api/fetchProducts'
 import { IProduct } from '@interfaces/Product.interface'
 export const useProductsStore = defineStore('productsStore', () => {
-    const products = ref([] as IProduct[])
-    const currentProduct = ref(null)
-    const status = ref<Boolean>(false)
-    const getProducts = async () => {
-        await fetchProducts().then(res => {
-            products.value = res?.data.items
-        })
-        status.value = true
-    }
-    return {
-        products,
-        status,
-        getProducts,
-        currentProduct
-    }
+	const products = ref([] as IProduct[])
+	const currentProduct = ref({} as IProduct)
+	const status = ref<Boolean>(false)
+	const setCurrentProduct = (product: IProduct) => {
+		currentProduct.value = product
+	}
+	const getProducts = async () => {
+		await fetchProducts().then((res) => {
+			products.value = res?.data.items
+		})
+		status.value = true
+	}
+	return {
+		products,
+		status,
+		getProducts,
+		setCurrentProduct,
+		currentProduct
+	}
 })
