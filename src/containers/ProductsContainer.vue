75c6929813384768cbcdebb2579d73d2e905ffec
Kirill Starodubsev <colereteps@gmail.com>
2023-03-23 18:46:51 +0400

    route added to products page

--- src/containers/ProductsContainer.vue
+++ src/containers/ProductsContainer.vue
@@ -1,6 +1,7 @@
 <template>
 	<div :class="$style.container">
 		<ItemProduct
+			@click="goToProduct(product)"
 			:product="product"
 			v-for="product in productsStore.products"
 		></ItemProduct>
@@ -11,8 +12,17 @@
 import { useProductsStore } from '@store/ProductsStore'
 import { onMounted } from 'vue'
 import ItemProduct from '@components/ItemProduct.vue'
-
+import { router } from '@routes/index'
+import { IProduct } from '@interfaces/Product.interface'
 const productsStore = useProductsStore()
+const goToProduct = (product: IProduct) => {
+	router.push({
+		name: 'product',
+		params: {
+			id: product.id
+		}
+	})
+}
 
 onMounted(async () => {
 	await productsStore.getProducts()
