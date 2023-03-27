2b760313f3134531d6e356006c528b64d5ad0cb4
Kirill Starodubsev <colereteps@gmail.com>
2023-03-27 16:17:27 +0400

    refactor

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,22 +6,27 @@
 			:products="productsStore.products"
 			style="margin-top: 10px"
 		/>
-		<Loading
-			v-if="!(productsStore.status && categoriesStore.status)"
-			:class="$style.loading"
-		/>
+		<Loading v-if="!loadingComplete" :class="$style.loading" />
 	</BaseLayout>
 </template>
 
 <script setup lang="ts">
-import Loading from '@components/Loading.vue'
+import { computed } from 'vue'
+import BaseLayout from '@layouts/BaseLayout.vue'
 import { useProductsStore } from '@store/ProductsStore'
 import { useCategoriesStore } from '@store/CategoriesStore'
 import ProductsContainer from '@containers/ProductsContainer.vue'
 import CategoriesContainer from '@containers/CategoriesContainer.vue'
-import BaseLayout from '@layouts/BaseLayout.vue'
+import Loading from '@components/Loading.vue'
+
 const categoriesStore = useCategoriesStore()
 const productsStore = useProductsStore()
+
+const productsLoaded = computed(() => productsStore.status)
+const categoriesLoaded = computed(() => categoriesStore.status)
+const loadingComplete = computed(
+	() => productsLoaded.value && categoriesLoaded.value
+)
 </script>
 
 <style lang="scss" module>
