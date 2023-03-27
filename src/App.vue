436289fd63c5bc592f75a70b593b922a13f0ac34
Kirill Starodubsev <colereteps@gmail.com>
2023-03-27 16:21:58 +0400

    removed unnecessary

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,5 @@
 <template>
-	<div id="app">
-		<div class="header">
-			<p>Mini Shop</p>
-			<ICartButton :quantity="5" />
-		</div>
-		<div class="main">
-			<Button :apperance="'unprimary'">Добавить</Button>
-			<Button :apperance="'primary'">Купить</Button>
-		</div>
-	</div>
+	<RouterView />
 </template>
 
-<script lang="ts" setup>
-import { defineComponent } from 'vue'
-import Button from '@components/Button.vue'
-import ICartButton from '@components/CartButton.vue'
-</script>
-
-<style lang="scss" scoped>
-@import '@scss/colors.scss';
-#app {
-	height: 100vh;
-}
-.header {
-	display: flex;
-	justify-content: space-between;
-	padding: 20px;
-	background-color: $black;
-	color: white;
-	align-items: center;
-}
-.main {
-	height: 100%;
-	background-color: $light;
-}
-</style>
+<script lang="ts" setup></script>
