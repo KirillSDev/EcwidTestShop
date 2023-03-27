fa76412ea49834b236bc3fae3b322363a5a1b0c3
Kirill Starodubsev <colereteps@gmail.com>
2023-03-27 14:13:29 +0400

    added types for RadioOption

--- src/interfaces/Product.interface.ts
+++ src/interfaces/Product.interface.ts
@@ -8,9 +8,25 @@ export interface IProduct {
 	description: string
 	categoryIds: number[]
 	galleryImages: IGalleryProductImages[]
+	options: IRadioOption[]
 }
 
 export interface IGalleryProductImages {
 	id: number
 	thumbnailUrl: string
+}
+
+export interface IRadioOption {
+	type: string
+	name: string
+	nameTranslated: { [key: string]: string }
+	choices: ProductOptionChoice[]
+	defaultChoice: number
+	required: boolean
+}
+interface ProductOptionChoice {
+	text: string
+	textTranslated: { [key: string]: string }
+	priceModifier: number
+	priceModifierType: string
 }
